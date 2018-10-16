bb202bcb7e04b77cef851c18e03140711c345af1
anvesh.vemula <anvesh.vemula@HYDHTC111604L.ad.infosys.com>
2018-10-17 01:25:19 +0530

    v1

---
+++ Sample/src/main/java/com/infy/Sample/SampleApplication.java
@@ -0,0 +1,13 @@
+package com.infy.Sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleApplication.class, args);
+		System.out.println("hello");
+	}
+}
